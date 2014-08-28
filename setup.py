db1979398b8ab06d9504aecaa71626564affe1a0
Phil Birkelbach <phil@petrasoft.net>
2014-08-28 11:24:19 -0500

    Initial Commit
    Older versions of this code can be found in the CAN-FIX Utility
    repository.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Python CANBus',
+    version='0.1.0',
+    packages=['canbus'],
+    long_description=open('README').read(),
+    license='GNU General Public License Version 2', 
+)
+
